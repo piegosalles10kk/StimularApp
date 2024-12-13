5de7f999b45dedf58d2766fbc03e9257ce088eb8
Diego Salles <104213637+piegosalles10kk@users.noreply.github.com>
2024-12-13 14:07:46 -0300

    <version-'>

--- src/servicos/api.ts
+++ src/servicos/api.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const link = 'http://167.88.33.130:3001'
 
-const link2 = "https://44a1b681-2e23-4bda-8bc0-060ec7663d49-00-39cxbndjc6z0c.kirk.replit.dev"
-
 const api = axios.create({
     baseURL: link,
 });
